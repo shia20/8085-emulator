82a6d43d14499bb8cdbf66ec5070322e9a66060e
Rishit <puligaddarishit@gmail.com>
2024-08-21 21:58:47 +0530

    Changed MOV to mov

--- Functions.py
+++ Functions.py
@@ -1,7 +1,7 @@
-def MOV(Rd, Rs):
+def mov(Rd, Rs):
     Rd = Rs
 
-def MOV(a, b, **kwargs):
+def mov(a, b, **kwargs):
     c = kwargs.get('c', 10)
     d = kwargs.get('d', 20)
     print(f"a: {a}, b: {b}, c: {c}, d: {d}")
